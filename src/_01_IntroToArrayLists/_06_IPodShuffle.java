8b52e8549660a29c793457fa6c89f2c6b9dc1fed
Sportsdawg1 <league@WorthyMini-6.attlocal.net>
2019-04-14 16:00:56 -0700

    stacks suck
    :stacks_suck:

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -13,10 +13,11 @@ import javax.swing.JPanel;
 //Copyright The League of Amazing Programmers, 2015
 
 public class _06_IPodShuffle implements ActionListener{
+	public int randomInt;
+	public ArrayList<Song> songs = new ArrayList<Song>();
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-		Random r = new Random();
+		
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -24,13 +25,12 @@ public class _06_IPodShuffle implements ActionListener{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		ArrayList<Song> songs = new ArrayList<Song>();
-		songs.add(new Song("/_01_Data_Structures/src/_01_IntroToArrayLists/ACDC - Thunderstruck (Official Music Video).mp3"));
-		songs.add(new Song("/_01_Data_Structures/src/_01_IntroToArrayLists/Miley Cyrus - Party In The U.S.A. (Official Music Video).mp3"));
+		
 	}
 	
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
+		_06_IPodShuffle shuffle = new _06_IPodShuffle();
+		shuffle.setup();
 	}
 	private void setup() {
 		// TODO Auto-generated method stub
@@ -39,11 +39,26 @@ public class _06_IPodShuffle implements ActionListener{
 		JButton button = new JButton("surprise");
 		panel.add(button);
 		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		button.addActionListener(this);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		Random r = new Random();
 		
+		songs.add(new Song("/Users/league/Desktop/level3-module1-Sportsdawg1/src/_01_IntroToArrayLists/ACDC - Thunderstruck (Official Music Video).mp3"));
+		songs.add(new Song("/Users/league/Desktop/level3-module1-Sportsdawg1/src/_01_IntroToArrayLists/Miley Cyrus - Party In The U.S.A. (Official Music Video).mp3"));
+		songs.add(new Song("/Users/league/Desktop/level3-module1-Sportsdawg1/src/_01_IntroToArrayLists/Immigrant Song (2007 Remaster).mp3"));
+		songs.add(new Song("/Users/league/Desktop/level3-module1-Sportsdawg1/src/_01_IntroToArrayLists/Journey - Don't Stop Believin' (Audio).mp3"));
+		songs.add(new Song("/Users/league/Desktop/level3-module1-Sportsdawg1/src/_01_IntroToArrayLists/Kenny Loggins - Danger Zone (Audio).mp3"));
+		songs.get(randomInt).stop();
+		for (int i = 0; i < songs.size(); i++) {
+			songs.get(i).stop();
+		}
+		randomInt = r.nextInt(5);
+		songs.get(randomInt).play();
 	}
 }
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,28 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> sd = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for (int i = 0; i < 100; i++) {
+			Random r = new Random();
+			sd.push(r.nextDouble() * 100);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		double num1 = Integer.parseInt(JOptionPane.showInputDialog("number"));
+		double num2 = Integer.parseInt(JOptionPane.showInputDialog("number 2"));
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		Double popped;
+		for (int i = 0; i < sd.size(); i++) {
+			popped = sd.pop();
+			if (popped > num1 && popped < num2) {
+				System.out.println(popped);
+			}
+			if (popped < num1 && popped > num2) {
+				System.out.println(popped);
+			}
+		System.out.println("test");
+		}
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
