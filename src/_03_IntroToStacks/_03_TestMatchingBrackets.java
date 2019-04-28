428777d44c6314d28022ddb540f8797eb1cda463
League <league@WTS-IM-17.attlocal.net>
2019-04-28 15:51:04 -0700

    whee started hangman
    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -20,8 +20,31 @@ public class _03_TestMatchingBrackets {
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		
-		return false;
+		Stack <Character> charS = new Stack<Character>();
+		Stack <Character> charSPushPop = new Stack<Character>();
+		char popped;
+		int count = 0;
+		for (int i = 0; i < b.length(); i++) {
+			charS.push(b.charAt(i));
+		}
+		for (int i = 0; i < b.length(); i++) {
+			charSPushPop.push(charS.pop());
+		}
+		for (int i = 0; i < b.length(); i++) {
+			popped = charSPushPop.pop();
+			if(popped == '{') {
+				count++;
+			} else {
+				count--;
+			}
+			if (count < 0) {
+				return false;
+			}
+		}
+		if(count != 0) {
+			return false;
+		}
+		return true;
 	}
 
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,46 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class HangMan{
-	
-	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel("6");
+	JLabel lives = new JLabel("");
+	int numLives = 6;
+	Utilities u = new Utilities();
+	String labelString = "";
+	Stack<String> strings = new Stack<String>();
+	public static void main(String[] args) {
+		HangMan h = new HangMan();
+		h.setup();
+	}
+	void setup() {
+		int num = howManyStrings();
+		panel.add(label);
+		panel.add(lives);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
+		for (int i = 0; i < num; i++) {
+			strings.push(u.readRandomLineFromFile("dictionary.txt"));
+		}
+		setupLabel(strings.pop());
+		label.setText(labelString);
+	}
+	int howManyStrings() {
+		String s = JOptionPane.showInputDialog("how many strings");
+		return Integer.parseInt(s);
+	}
+	void setupLabel(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			labelString = labelString + "_";
+		}
+	}
 	
 }
